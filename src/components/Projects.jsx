a35443d3ee67fd9112cf3a279909300962809714
Dinesh Sainath <sainath.dinesh@gmail.com>
2025-03-09 03:23:18 +0530

    project github update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import { MoveUpRight } from "lucide-react";
+import { RxGithubLogo } from "react-icons/rx";
 
 function Projects() {
   return (
@@ -14,22 +15,26 @@ function Projects() {
             description:
               "An AI-generated color palette application from user's prompt",
             link: `https://ai-color-palette.onrender.com/`,
+            github: `https://github.com/dineshsainath/ai-color-palette`,
           },
           {
             name: "Wedding Wise",
             description:
               "A wedding planning application with vendor management.",
             link: `https://weddingwise-ds.netlify.app/`,
+            github: `https://github.com/dineshsainath/weddingwise`,
           },
           {
             name: "Profile Deck",
             description: "An app for managing user profiles and settings.",
             link: `https://super-macaron-b74cb9.netlify.app/login`,
+            github: `https://github.com/DineshSainath/student-profile-2114`,
           },
           {
             name: "Keeper",
             description: "A note-taking app inspired by Google Keep.",
             link: `https://coruscating-crostata-d691f8.netlify.app/`,
+            github: `https://github.com/dineshsainath/Keeper`,
           },
         ].map((project) => (
           <div
@@ -45,6 +50,13 @@ function Projects() {
               <div>
                 <h4 className="project-heading fw-bold mb-1">{project.name}</h4>{" "}
                 <MoveUpRight className="link-icon" size={24} />
+                <a
+                  href={project.github}
+                  target="_blank"
+                  rel={"noopener noreferrer"}
+                >
+                  <RxGithubLogo className="projectIcon" size={30} />
+                </a>
                 <p className="description text-gray-500 mb-0">
                   {project.description}
                 </p>
